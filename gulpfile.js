9f7aa178f1a08ad2c33b38df6864569df7ec94a2
NoDreams2 <lykyanov2005@mail.ru>
2024-11-25 08:39:00 +0700

    Шапка + главная секция

---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+const gulp = require("gulp");
+const concat = require("gulp-concat-css");
+const plumber = require("gulp-plumber");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const mediaquery = require("postcss-combine-media-query");
+const cssnano = require("cssnano");
+const htmlMinify = require("html-minifier");
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require("gulp-uglify");
+const concatGulp = require("gulp-concat");
+const order = require("gulp-order");
+
+const copy = require("gulp-copy");
+
+// Start server
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+}
+
+function pagesScss() {
+  const plugins = [autoprefixer(), mediaquery(), cssnano()];
+  return gulp
+    .src("src/pages/**/*.scss")
+    .pipe(sass())
+    .pipe(concat("bundle.css"))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Pipe file.html
+function html() {
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    keepClosingSlash: true,
+  };
+  return gulp
+    .src("src/pages/**/*.html")
+    .pipe(plumber())
+    .on("data", function (file) {
+      const buferFile = Buffer.from(
+        htmlMinify.minify(file.contents.toString(), options)
+      );
+      return (file.contents = buferFile);
+    })
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Pipe scripts.js
+function scripts() {
+  return gulp
+    .src(["src/js/**/*.js"])
+    .pipe(concatGulp("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Pipe images
+function images() {
+  return gulp
+    .src("src/**/*.{jpg,png,svg,gif,ico,webp,avif,mp4,ogv}", {
+      encoding: false,
+    })
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Pipe fonts
+function fonts() {
+  return gulp
+    .src("src/common/fonts/**/*.{woff,woff2,ttf}")
+    .pipe(gulp.dest("dist/fonts"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Pipe libs
+const swiperDir = 'node_modules/swiper/'
+function copySwiper() {
+  return gulp.src([
+    swiperDir + 'swiper-bundle.min.js',
+    swiperDir + 'swiper-bundle.min.css'
+  ])
+    .pipe(gulp.dest('dist/libs/swiper/'))
+}
+
+const gsapDir = 'node_modules/gsap/dist/';
+function copyGsap() {
+  return gulp.src([
+    gsapDir + 'ScrollTrigger.min.js'
+  ])
+    .pipe(gulp.dest('dist/libs/gsap/'))
+}
+
+const bootstrapDir = 'node_modules/bootstrap/dist/'; 
+function copyBootstrap() {
+  return gulp.src([
+    bootstrapDir + 'css/bootstrap.min.css', 
+    bootstrapDir + 'js/bootstrap.bundle.min.js', 
+  ])
+    .pipe(gulp.dest('dist/libs/bootstrap/')); 
+}
+
+// File monitoring
+function watchFiles() {
+  gulp.watch(["src/**/*.html"], html);
+  gulp.watch(["src/**/*.scss"], pagesScss);
+  gulp.watch(["src/**/*.js"], scripts);
+}
+
+// Clean dev dist
+function clean() {
+  return del("dist");
+}
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, pagesScss, scripts, fonts, images, copyBootstrap, copyGsap, copySwiper)
+);
+const watchapp = gulp.parallel(build, watchFiles, serve);
+
+// Command in console
+exports.html = html;
+exports.pagesScss = pagesScss;
+exports.images = images;
+exports.clean = clean;
+exports.scripts = scripts;
+exports.fonts = fonts;
+
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
